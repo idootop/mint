76f3d1707def68efea3fa0aabc7e5e31b594871a
WJG <wjg12341234@163.com>
2023-11-01 00:05:15 +0800

    misc: refine BaseImage's layout

--- src/components/Image/BaseImage.tsx
+++ src/components/Image/BaseImage.tsx
@@ -2,6 +2,8 @@
 
 import { isNotEmpty } from '@/core/utils/is';
 
+import styles from './styles.module.css';
+
 interface ImageProps {
   src?: string;
   alt?: string;
@@ -11,27 +13,9 @@ export const BaseImage = (props: ImageProps) => {
   const { src, alt } = props;
 
   return (
-    <span
-      style={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <img src={src} alt={alt} />
-      {isNotEmpty(alt) && (
-        <span
-          style={{
-            color: '#666',
-            fontSize: '12px',
-            textAlign: 'center',
-            marginTop: '10px',
-          }}
-        >
-          {alt}
-        </span>
-      )}
-    </span>
+    <>
+      <img src={src} alt={alt} className={styles.center_image} />
+      {isNotEmpty(alt) && <span className={styles.center_label}>{alt}</span>}
+    </>
   );
 };
