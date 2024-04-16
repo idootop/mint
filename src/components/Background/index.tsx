eab16aae702c562226480875bd614ec25d9302fd
WJG <wjg12341234@163.com>
2024-04-16 23:57:31 +0800

    refactor: cache rock images

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { Box } from '@/common/components/Box';
 import { Image } from '@/common/components/Image';
@@ -12,6 +12,13 @@ import rock3 from './images/rock3.svg';
 
 export function Background({ children, isMobile, height }) {
   const count = isMobile ? 40 : 100;
+  const rocks = useMemo(() => {
+    return [rock1, rock2, rock3].map(rock => {
+      const img = new window.Image();
+      img.src = rock.src;
+      return img;
+    });
+  }, []);
   return (
     <Stack width="100%" height={height} overflow="hidden">
       <Box size="100%" />
@@ -20,6 +27,7 @@ export function Background({ children, isMobile, height }) {
           <Rock
             key={idx}
             config={{
+              rocks,
               depthRange: isMobile ? 50 : 50,
               moveSpeed: isMobile ? 2 : 2,
               rotateSpeed: isMobile ? 1 : 1,
@@ -38,7 +46,7 @@ interface RockContext {
   /**
    * SVG 图片
    */
-  rock: string;
+  rock: HTMLImageElement;
   x: number;
   y: number;
   size: number;
@@ -51,9 +59,14 @@ interface RockContext {
 }
 
 const Rock = (props: {
-  config: { depthRange: number; moveSpeed: number; rotateSpeed: number };
+  config: {
+    rocks: HTMLImageElement[];
+    depthRange: number;
+    moveSpeed: number;
+    rotateSpeed: number;
+  };
 }) => {
-  const { depthRange, moveSpeed, rotateSpeed } = props.config;
+  const { rocks, depthRange, moveSpeed, rotateSpeed } = props.config;
   const easing = x => Math.sqrt(1 - Math.pow(x - 1, 2));
   const randomX = () => randomFloat(0, 100);
   const randomY = () => randomFloat(0, 3 * 100);
@@ -64,7 +77,7 @@ const Rock = (props: {
     y: randomY(),
     rotate: Math.random() * Math.PI * rotateSpeed,
     // 不变量
-    rock: pickOne([rock1, rock2, rock3])!.src,
+    rock: pickOne(rocks)!,
     size: randomZ(depthRange) * 12,
     moveSpeed: Math.random() * moveSpeed,
     rotateSpeed: Math.random() * Math.PI * rotateSpeed,
@@ -109,7 +122,7 @@ const Rock = (props: {
   const { x, y, size, rock } = state;
   return (
     <Position left={`${x}%`} top={`${y}%`}>
-      <Image src={rock} alt="rock" size={size} />
+      <Image src={rock.src} alt="rock" size={size} objectFit="contain" />
     </Position>
   );
 };
