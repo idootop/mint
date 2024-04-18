6f628510697678d6192cd6721edf10d94255e4fa
WJG <wjg12341234@163.com>
2024-04-18 23:24:36 +0800

    feat: add typing effect for hello text

--- src/layouts/Home/Hello/index.tsx
+++ src/layouts/Home/Hello/index.tsx
@@ -1,11 +1,62 @@
-import { Center } from '@/common/components/Flex';
+'use client';
 
+import { useEffect, useRef } from 'react';
+
+import { Center } from '@/common/components/Flex';
+import { sleep } from '@/common/utils/base';
+
+import { hellos } from './hello';
 import styles from './styles.module.css';
 
 export const Hello = () => {
+  const initRef = useRef(0);
+  useEffect(() => {
+    initRef.current = initRef.current + 1;
+    const currentInit = initRef.current;
+
+    let timer;
+    let languageIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+
+    async function type() {
+      if (initRef.current !== currentInit) {
+        return;
+      }
+      const text = hellos[languageIndex];
+      const speed = isDeleting ? 50 : 150;
+
+      if (!isDeleting && charIndex === text.length) {
+        setTimeout(() => {
+          isDeleting = true;
+        }, 1000);
+      } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        languageIndex = (languageIndex + 1) % hellos.length;
+        await sleep(200);
+      }
+
+      const e = document.getElementsByClassName(styles['hello'])[0].children[0];
+      e.textContent = isDeleting
+        ? text.substring(0, charIndex - 1)
+        : text.substring(0, charIndex + 1);
+
+      charIndex += isDeleting ? -1 : 1;
+      timer = setTimeout(type, speed);
+    }
+
+    type();
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
   return (
     <Center height="100%">
-      <h1 className={styles.hello}>你好</h1>
+      <div className={styles.hello}>
+        <span />
+        <div />
+      </div>
     </Center>
   );
 };
