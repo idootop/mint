e36a5bebdad9fcb1b866b9c29041e9b1392de095
WJG <wjg12341234@163.com>
2024-05-17 09:01:28 +0800

    doc: update develop projects' description

--- app/projects/develop/ts-json-object/page.tsx
+++
@@ -1,7 +0,0 @@
-import { generateProjectPage } from '../../_project';
-import Content from './content.mdx';
-
-// @ts-ignore
-const p = await generateProjectPage(import.meta.url, Content);
-export const metadata = p.metadata;
-export default p.Page;
