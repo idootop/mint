88cbdefa987f6e94d614f4c335786c72c5372c6a
WJG <wjg12341234@163.com>
2024-11-27 23:22:42 +0800

    feat: add async-tasks

--- src/components/Code/CodeCard.tsx
+++ src/components/Code/CodeCard.tsx
@@ -10,7 +10,7 @@ export const CodeCard = ({ children }) => {
         <div className={styles.yellow} />
         <div className={styles.green} />
       </Row>
-      <pre>{children}</pre>
+      <pre className="hide-scrollbar">{children}</pre>
     </Column>
   );
 };
