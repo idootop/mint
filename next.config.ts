a5ae7ba08099f715edbc7a04f694a6ce64185ac7
Del Wang <wjg12341234@163.com>
2026-06-01 00:16:21 +0800

    feat: 更新到 nextjs 16 支持 turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,23 @@
 import configMDX from '@next/mdx';
 import createNextFileLoader from 'next-file-loader';
 
-const withNextFileLoader = createNextFileLoader([
-  {
-    test: /\.(mp4|webm|mkv|ogv|wmv|avi|mov|flv|m4v|3gp)$/i,
-    outputPath: 'static/videos/[name].[hash:8].[ext]',
-  },
-  {
-    test: /\.(mp3|wav|flac|ogg|aac|m4a|wma|ape)$/i,
-    outputPath: 'static/audios/[name].[hash:8].[ext]',
-  },
-  {
-    test: /\.(glb|gltf)$/i,
-    outputPath: 'static/models/[name].[hash:8].[ext]',
-  },
-]);
+const withNextFileLoader = createNextFileLoader(
+  [
+    {
+      test: /\.(mp4|webm|mkv|ogv|wmv|avi|mov|flv|m4v|3gp)$/i,
+      outputPath: 'static/videos/[name].[hash:8].[ext]',
+    },
+    {
+      test: /\.(mp3|wav|flac|ogg|aac|m4a|wma|ape)$/i,
+      outputPath: 'static/audios/[name].[hash:8].[ext]',
+    },
+    {
+      test: /\.(glb|gltf)$/i,
+      outputPath: 'static/models/[name].[hash:8].[ext]',
+    },
+  ],
+  { ignoreWarnings: true },
+);
 
 const withMDX = configMDX({
   extension: /\.mdx?$/,
