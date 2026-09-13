82bcec10f4aa435c35c1c7da291dfa571dfe5a07
Del Wang <wjg12341234@163.com>
2026-09-13 20:14:56 +0800

    feat: 新增 doubless

---
+++ app/projects/product/doubless/meta.ts
@@ -0,0 +1,11 @@
+import cover from './cover.png';
+
+export const metadata = {
+  emoji: '🧹',
+  title: 'Doubless',
+  source: 'https://github.com/idootop/doubless',
+  createAt: '2026-08',
+  description: '免费、开源、跨平台的重复文件查找与清理工具',
+  cover: cover.src,
+  pinned: true,
+};
