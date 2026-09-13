7399a8bedb3ebb801d30502a1b4bf244023f146d
Del Wang <wjg12341234@163.com>
2026-09-13 18:58:20 +0800

    feat(project): 新增 CCPulse

---
+++ app/projects/tool/ccpulse/meta.ts
@@ -0,0 +1,7 @@
+export const metadata = {
+  emoji: '💓',
+  title: 'CCPulse',
+  source: 'https://github.com/idootop/ccpulse',
+  createAt: '2026-07',
+  description: 'Your Claude Code usage, at a pulse.',
+};
