0dcf89bbb2290d2fe1b82db5e59514617a1bcad8
Del Wang <wjg12341234@163.com>
2026-09-13 19:49:14 +0800

    feat: 新增 echostream

---
+++ app/projects/package/echostream/meta.ts
@@ -0,0 +1,7 @@
+export const metadata = {
+  emoji: '🌊',
+  title: 'EchoStream',
+  source: 'https://github.com/del-wang/echostream',
+  createAt: '2026-01',
+  description: 'A bi-directional RPC and streaming framework',
+};
