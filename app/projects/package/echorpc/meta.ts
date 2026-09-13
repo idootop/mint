4e9600340b36bc09aacb99e9058f1f6ebd1e2998
Del Wang <wjg12341234@163.com>
2026-09-13 19:25:00 +0800

    feat: 新增 echorpc

---
+++ app/projects/package/echorpc/meta.ts
@@ -0,0 +1,7 @@
+export const metadata = {
+  emoji: '🐚',
+  title: 'EchoRPC',
+  source: 'https://github.com/del-wang/echorpc',
+  createAt: '2026-03',
+  description: 'Bidirectional JSON-RPC 2.0 over WebSocket',
+};
