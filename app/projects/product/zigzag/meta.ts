328f58dbd42af9e8002e33104d113fa397a41bd2
Del Wang <wjg12341234@163.com>
2026-09-13 20:50:10 +0800

    feat: 新增 zigzag

---
+++ app/projects/product/zigzag/meta.ts
@@ -0,0 +1,11 @@
+import cover from './cover.png';
+
+export const metadata = {
+  emoji: '💾',
+  title: 'ZigZag',
+  source: 'https://github.com/idootop/zigzag',
+  createAt: '2026-08',
+  description: '一款面向本地归档的多媒体批量压缩工具',
+  cover: cover.src,
+  pinned: true,
+};
