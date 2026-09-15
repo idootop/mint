42d4eb2dcb0a7d92b9442f86cf842fde4af58a9c
Del Wang <wjg12341234@163.com>
2026-09-15 21:58:20 +0800

    feat: 新增 Miloco

---
+++ app/projects/work/miloco/meta.ts
@@ -0,0 +1,11 @@
+import cover from './cover.png';
+
+export const metadata = {
+  emoji: '🏠',
+  title: 'Xiaomi Miloco',
+  source: 'https://github.com/XiaoMi/xiaomi-miloco',
+  createAt: '2026-03',
+  description: '小米面向未来的全屋智能 AI 探索方案',
+  cover: cover.src,
+  pinned: true,
+};
