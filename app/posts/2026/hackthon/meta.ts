4eea28aa53b89ce2ccb0e695e3ea8c4d5421205f
Del Wang <wjg12341234@163.com>
2026-09-19 19:16:08 +0800

    feat: 柏林 IFA 之旅

--- app/posts/2026/hackthon/meta.ts
+++ app/posts/2026/hackthon/meta.ts
@@ -1,5 +1,5 @@
 export const metadata = {
   title: '小米黑客马拉松',
-  description: '参加第七届小米集团黑客马拉松，获得二等奖（第 3 名）',
+  description: '参加小米集团第七届黑客马拉松，获得二等奖（第 3 名）',
   createAt: '2026-08-10',
 };
---
+++ app/posts/2026/ifa/meta.ts
@@ -0,0 +1,5 @@
+export const metadata = {
+  title: '柏林 IFA 之旅',
+  description: '第一次出国参加柏林 IFA 展会',
+  createAt: '2026-09-10',
+};
